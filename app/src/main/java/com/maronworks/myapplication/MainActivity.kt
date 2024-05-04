5a5aba321ca903bf79ec5ed52ff3fc01f7f4002c
ralphmaron <ralphmaron.dev@gmail.com>
2024-05-04 14:08:40 +0800

    added splash screen

--- app/src/main/java/com/maronworks/myapplication/MainActivity.kt
+++ app/src/main/java/com/maronworks/myapplication/MainActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             MyApplication()
